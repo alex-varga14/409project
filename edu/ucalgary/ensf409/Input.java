38cd4550fad7a4f09fb1d5a54e476af7c35d1783
Kjeon7 <72672852+Kjeon7@users.noreply.github.com>
2021-04-08 18:33:27 -0600

    Actually works

--- edu/ucalgary/ensf409/Input.java
+++ edu/ucalgary/ensf409/Input.java
@@ -4,6 +4,7 @@ import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;  
+import java.util.concurrent.CountDownLatch;
 /**
  * ENSF409 FINAL PROJECT GROUP 40
  * Authors:
@@ -18,7 +19,6 @@ Basic for now, implementation can be made better and more efficient possibly.
 */
 //**** NOT FINISHED */
 public class Input extends JFrame implements ActionListener {
-
     JTextField in; // Initialize a textfield that allows for user input in the GUI.
     JLabel l;      // Initialize a label that gives the user directions on what to input.
     JButton b;     // Initialize a button that can be clicked on.
@@ -29,6 +29,7 @@ public class Input extends JFrame implements ActionListener {
     private String input2;
     private String input3;
     private String regex = "[0-9]+";
+    public CountDownLatch loginSignal = new CountDownLatch(1);
 
     public Input(){ // sets up GUI
         in = new JTextField();
@@ -61,7 +62,7 @@ public class Input extends JFrame implements ActionListener {
 
             if(counter == 1){
                 input1 = in.getText();
-                input1 = input1.toLowerCase();
+                input1 = input1.toLowerCase().strip();
                 if (input1.compareTo("kneeling") == 0 || input1.compareTo("task") == 0 || input1.compareTo("mesh") == 0 || input1.compareTo("executive") == 0  || input1.compareTo("ergonomic") == 0  || input1.compareTo("standing") == 0 ||input1.compareTo("adjustable") == 0 ||input1.compareTo("traditional") == 0  || input1.compareTo("desk") == 0  || input1.compareTo("study") == 0  || input1.compareTo("swing Arm") == 0 || input1.compareTo("small") == 0 || input1.compareTo("medium") == 0 || input1.compareTo("large") == 0){
                 input = input1;
                 l.setText("Now Choose a furniture type:");
@@ -81,9 +82,9 @@ public class Input extends JFrame implements ActionListener {
 
             else if (counter == 2){
                 input2 = in.getText();
-                input2 = input2.toLowerCase();
+                input2 = input2.toLowerCase().strip();
                 if (input1.compareTo("kneeling") == 0 && input2.compareTo("chair") == 0|| input1.compareTo("task") == 0 && input2.compareTo("chair") == 0|| input1.compareTo("mesh") == 0 && input2.compareTo("chair") == 0 || input1.compareTo("executive") == 0 && input2.compareTo("chair") == 0 || input1.compareTo("ergonomic") == 0 && input2.compareTo("chair") == 0 || input1.compareTo("standing") == 0 && input2.compareTo("desk") == 0 ||input1.compareTo("adjustable") == 0 && input2.compareTo("desk") == 0 ||input1.compareTo("traditional") == 0 && input2.compareTo("desk") == 0  || input1.compareTo("desk") == 0 && input2.compareTo("lamp") == 0 || input1.compareTo("study") == 0 && input2.compareTo("lamp") == 0   || input1.compareTo("swing Arm") == 0  && input2.compareTo("lamp") == 0 || input1.compareTo("small") == 0  && input2.compareTo("filing") == 0  || input1.compareTo("medium") == 0 && input2.compareTo("filing") == 0 || input1.compareTo("large") == 0 && input2.compareTo("filing") == 0 ){
-                input += " " + in.getText();
+                input += " " + input2;
                 l.setText("Finally, please request the amount of the specified item you want:");
                 in.setText("");
                 counter = 3;
@@ -97,11 +98,12 @@ public class Input extends JFrame implements ActionListener {
             }
 
             else if (counter == 3){
-                input3 = in.getText();
+                input3 = in.getText().strip();
                 if (input3.matches(regex)){
-                input += ", " + in.getText();
+                input += ", " + input3;
                 l.setText("Order Complete! Click enter for new order");
                 System.out.println(input);
+                loginSignal.countDown();
 
                 counter = 4;
                 }
@@ -148,20 +150,22 @@ public class Input extends JFrame implements ActionListener {
        * OrderArugmentNotProvidedException. Additional arguments are ignored.
 	  */
 
-	public static void main(String[] args) { //throws OrderArgumentNotProvidedException {
+	public static void main(String[] args) throws InterruptedException { //throws OrderArgumentNotProvidedException {
+
            Input input = new Input(); 
+           input.loginSignal.await();
 
            // StringBuilder tmp = new StringBuilder();
            //tmp.append(args[0].strip() + " " + args[1].strip() + " " + args[2].strip());
-            //Inventory inventory = new Inventory();
-            //inventory.initializeConnection();
+            Inventory inventory = new Inventory();
+            inventory.initializeConnection();
 			//Order example = new Order(input.getInput());
 
            //if (input.getInput == null) {
                 //throw new OrderArgumentNotProvidedException();
            // }
             
-            //String result = inventory.executeOrder(example);
+           // String result = inventory.executeOrder(example);
        
               // System.out.println(input.getInput());
            
